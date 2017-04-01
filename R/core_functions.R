99e3f81880efe96fc3c76a7334da2ea96c91e62f
Anthony Ebert <anthonyebert@gmail.com>
2017-04-01 23:33:31 +1000

    make sure url_convert can handle character vector. Improve documentation.

--- R/core_functions.R
+++ R/core_functions.R
@@ -8,7 +8,9 @@
 #' aec_website <- "http://www.aec.gov.au/Electorates/gis/gis_datadownload.htm"
 #' find_files(aec_website, "zip")
 #' find_files(aec_website, "zip") %>% url_convert(aec_website)
+#' \dontrun{
 #' (find_files(aec_website, "zip") %>% url_convert(aec_website))[c(1,2)] %>% download.files()
+#' }
 #' @return Character vector of URLs
 #' @export
 find_files <- function(url, extension){
@@ -17,33 +19,36 @@ find_files <- function(url, extension){
 
   pg <- xml2::read_html(url)
   fl <- pg %>% rvest::html_nodes("a") %>% rvest::html_attr("href")
-  vector_for_url<- fl[grep(ext_pattern, fl)]
 
-  real_files <- fl[grep(ext_pattern, fl)]
-  vector_for_user <- basename(real_files)
-
-  file_url <- real_files
+  file_url <- fl[grep(ext_pattern, fl)]
+  attr(file_url, "base") = url
 
   return(file_url)
 }
 
-#' Convert relative URLs to absolute URLs
+#' Convert URLs (relative or absolute) to absolute URLs
 #' @export
 #' @param url string of URL
 #' @param base string of domain URL
 #' @examples
 #' aec_website <- "http://www.aec.gov.au/Electorates/gis/gis_datadownload.htm"
 #' find_files(aec_website, "zip") %>% url_convert(aec_website)
+#' find_files("https://catalog.data.gov/dataset?metadata_type=geospatial&_metadata_type_limit=0&q=%22Congressional%20Districts%22", "zip") %>% url_convert()
 #' @return character vector of absolute URLs
-url_convert <- function(url, base){
+url_convert <- function(url, base = attr(url, "base")){
 
-  htx <- urltools::scheme(url)
-  if(is.na(htx) == FALSE){
-    output_url <- url
-  } else {
-    full_domain <- paste(dirname(base), "/", sep = "")
-    output_url <- xml2::url_absolute(url, full_domain)
+  output_url <- rep(NA, length(url))
+
+  for(i in 1:length(url)){
+    htx <- urltools::scheme(url[i])
+    if(is.na(htx) == FALSE){
+      output_url[i] <- url[i]
+    } else {
+      full_domain <- paste(dirname(base), "/", sep = "")
+      output_url[i] <- xml2::url_absolute(url[i], full_domain)
+    }
   }
+
   return(output_url)
 }
 
