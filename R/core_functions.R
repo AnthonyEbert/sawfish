22649fabd011323a491a8c7dafbbd12d1267a53c
AnthonyEbert <anthonyebert@gmail.com>
2019-03-04 12:22:45 +1000

    Code cleanup. Version 0.1.0

--- R/core_functions.R
+++ R/core_functions.R
@@ -2,26 +2,32 @@
 #' List all files with a certain extension from a URL
 #' @importFrom magrittr %>%
 #' @param url string of URL
-#' @param extension string of file extension required (without the dot)
+#' @param extension string of file extension required (without the dot). To get all files set to \code{NULL}.
+#' @param url_convert boolean. If \code{TRUE} then relative URLs are converted to absolute URLs
 #' @examples
 #' find_files("http://www.abc.net.au/", "htm")
 #' aec_website <- "http://www.aec.gov.au/Electorates/gis/gis_datadownload.htm"
 #' find_files(aec_website, "zip")
-#' find_files(aec_website, "zip") %>% url_convert(aec_website)
 #' \dontrun{
-#' (find_files(aec_website, "zip") %>% url_convert(aec_website))[c(1,2)] %>% download.files()
+#' find_files(aec_website, "zip") %>% .[c(1,2)] %>% download.file(destfile = basename(.))
 #' }
 #' @return Character vector of URLs
 #' @export
-find_files <- function(url, extension){
-  ext_pattern <- paste("*.", extension, sep = "")
-  stem <- dirname(url)
-
+find_files <- function(url, extension, url_convert = TRUE){
   pg <- xml2::read_html(url)
   fl <- pg %>% rvest::html_nodes("a") %>% rvest::html_attr("href")
 
-  file_url <- fl[grep(ext_pattern, fl)]
+  if(!is.null(extension)){
+    file_url <- fl[which(tools::file_ext(fl) == extension)]
+  } else {
+    file_url <- fl
+  }
+
   attr(file_url, "base") = url
+
+  if(url_convert){
+    file_url <- url_convert(file_url)
+  }
 
   return(file_url)
 }
@@ -44,23 +50,16 @@ url_convert <- function(url, base = attr(url, "base")){
     if(is.na(htx) == FALSE){
       output_url[i] <- url[i]
     } else {
-      full_domain <- paste(dirname(base), "/", sep = "")
+      if(tools::file_ext(base) == ""){
+        full_domain <- base
+      } else {
+        full_domain <- paste(dirname(base), "/", sep = "")
+      }
       output_url[i] <- xml2::url_absolute(url[i], full_domain)
     }
   }
 
   return(output_url)
-}
-
-#' Download multiple files given a list of URLs
-#' @export
-#' @param url character vector of URLs
-#' @param destfile character vector of resulting filenames
-#' @return VOID
-download.files <- function(url, destfile = basename(url)){
-  for(i in 1:length(url)){
-    download.file(url[i], destfile[i])
-  }
 }
 
 
