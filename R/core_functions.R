a30789fae61896495e7d0efcefb3c522dfe7526c
Anthony Ebert <anthonyebert@gmail.com>
2020-01-31 16:57:33 +0100

    Version 0.1.1
    Changes to make sawfish simpler to use.
    
    find_files() now converts to absolute urls by default. This means that users don't need to know about url_convert().
    
    download.files() now passes optional arguments to download.file(). The reason such a simple function, instead of just using download.file() with lapply(), is that download.file() also requires a vector input for the destfile argument.
    
    http://www.abc.net.au/ removed since it no longer provides a good example.

--- R/core_functions.R
+++ R/core_functions.R
@@ -3,17 +3,16 @@
 #' @importFrom magrittr %>%
 #' @param url string of URL
 #' @param extension string of file extension required (without the dot)
+#' @param absolute logical should relative URLs be converted to absolute URLs?
 #' @examples
-#' find_files("http://www.abc.net.au/", "htm")
 #' aec_website <- "http://www.aec.gov.au/Electorates/gis/gis_datadownload.htm"
 #' find_files(aec_website, "zip")
-#' find_files(aec_website, "zip") %>% url_convert(aec_website)
 #' \dontrun{
-#' (find_files(aec_website, "zip") %>% url_convert(aec_website))[c(1,2)] %>% download.files()
+#' find_files(aec_website, "zip")[c(1,2)] %>% download.files()
 #' }
 #' @return Character vector of URLs
 #' @export
-find_files <- function(url, extension){
+find_files <- function(url, extension, absolute = TRUE){
   ext_pattern <- paste("*.", extension, sep = "")
   stem <- dirname(url)
 
@@ -22,6 +21,10 @@ find_files <- function(url, extension){
 
   file_url <- fl[grep(ext_pattern, fl)]
   attr(file_url, "base") = url
+
+  if(absolute){
+    file_url <- url_convert(file_url)
+  }
 
   return(file_url)
 }
@@ -32,9 +35,8 @@ find_files <- function(url, extension){
 #' @param base string of domain URL
 #' @examples
 #' aec_website <- "http://www.aec.gov.au/Electorates/gis/gis_datadownload.htm"
-#' find_files(aec_website, "zip") %>% url_convert(aec_website)
-#' find_files("https://catalog.data.gov/dataset?metadata_type=geospatial&_metadata_type_limit=0&q=%22Congressional%20Districts%22", "zip") %>% url_convert()
-#' @return character vector of absolute URLs
+#' find_files(aec_website, "zip")
+#' find_files("https://catalog.data.gov/dataset?metadata_type=geospatial&_metadata_type_limit=0&q=%22Congressional%20Districts%22", "zip")
 url_convert <- function(url, base = attr(url, "base")){
 
   output_url <- rep(NA, length(url))
@@ -49,17 +51,20 @@ url_convert <- function(url, base = attr(url, "base")){
     }
   }
 
+  attr(output_url, "base") <- base
+
   return(output_url)
 }
 
 #' Download multiple files given a list of URLs
 #' @export
-#' @param url character vector of URLs
+#' @param url character vector of URLs from \code{\link{find_files}}
 #' @param destfile character vector of resulting filenames
-#' @return VOID
-download.files <- function(url, destfile = basename(url)){
+#' @param ... arguments passed to \code{\link{download.file}}
+#' @return VOID no value returned in R. It does download the files however!
+download.files <- function(url, destfile = basename(url), ...){
   for(i in 1:length(url)){
-    download.file(url[i], destfile[i])
+    download.file(url[i], destfile[i], ...)
   }
 }
 
